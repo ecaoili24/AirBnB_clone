7fd2dd6ad219abca066616d7fe122880bba1201e
ecaoili24 <erika.caoili@gmail.com>
2020-02-15 00:34:51 +0000

    added a str to cast the uuid

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
 
     def __init__(self):
         """ initializes the BaseModel """
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 

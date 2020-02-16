b45bdfe2a2d7a0cda14a77de27df3a86d527d280
ecaoili24 <erika.caoili@gmail.com>
2020-02-16 22:50:14 +0000

    initial commit of test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,11 +1,2 @@
 #!/usr/bin/python3
-"""Unit Test fo BaseModel class"""
-import unittest
-from datetime import datetime
-from models.base_model import BaseModel
-import uuid
-
-class TestBaseModel(unittest.TestCase):
-    """A test for BaseModel class"""
-
-    def test_default_attr(self):
+"""Test for BaseModel class"""

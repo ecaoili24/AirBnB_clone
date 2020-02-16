bb773a48366323045fd16a6334e8a12d1a331ac5
ariana124 <ariana.bibiano@gmail.com>
2020-02-16 23:29:04 +0000

    updated base model for setting attributes

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,21 @@ import uuid
 class BaseModel:
     """ base model used for other classes """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initializes the BaseModel """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ prints the string representation of the BaseModel """

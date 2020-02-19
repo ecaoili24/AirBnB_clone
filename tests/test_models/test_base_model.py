8372ba62e4212d568e7ca22007cd25338a6a7780
ariana124 <ariana.bibiano@gmail.com>
2020-02-19 23:14:38 +0000

    added more tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,11 +2,12 @@
 """
 Test module for the class BaseModel
 """
+from datetime import datetime
 from models.base_model import BaseModel
 import unittest
 
 
-class TestBaseM(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         """ sets up an instance of a BaseModel """
@@ -40,4 +41,21 @@ class TestBaseM(unittest.TestCase):
         self.assertEqual(created, created2)
         self.assertNotEqual(updated, updated2)
 
-    
+    def test_str(self):
+        """ test to check the string representation """
+        m1 = BaseModel()
+        m1.name = "Pepe"
+        string = "[{}] ({}) {}".format(m1.__class__.__name__,
+                                       m1.id,
+                                       m1.__dict__)
+        self.assertEqual(str(m1), string)
+
+    def test_format(self):
+        """ test to check for time format """
+        m1 = BaseModel()
+        m1.save()
+        m1_json = m1.to_dict()
+        updated = m1.updated_at
+        updated2 = datetime.strptime(m1_json["updated_at"],
+                                     "%Y-%m-%dT%H:%M:%S.%f")
+        self.assertEqual(updated, updated2)

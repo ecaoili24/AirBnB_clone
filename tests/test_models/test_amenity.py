1ebfd1b9796d995cfdf7ff4a6b08b69b08c6ab74
ariana124 <ariana.bibiano@gmail.com>
2020-02-20 00:08:19 +0000

    changed amenity attribute test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -21,23 +21,13 @@ class TestAmenity(unittest.TestCase):
         """ tests to make sure both instances have different ids """
         a1 = Amenity()
         a2 = Amenity()
-        self.assertNotEqual(c1.id, c2.id)
+        self.assertNotEqual(a1.id, a2.id)
 
     def test_attributes(self):
-        """ tests attributes and save/update times """
+        """ tests attributes """
         a1 = Amenity()
         a1.name = "Bedroom"
-
-        self.assertTrue(c1.name, "Bedroom")
-
-        created = a1.created_at
-        updated = a1.updated_at
-        a1.save()
-        created2 = a1.created_at
-        updated2 = a1.updated_at
-
-        self.assertEqual(created, created2)
-        self.assertNotEqual(updated, updated2)
+        self.assertIsInstance(a1, string)
 
     def test_str(self):
         """ test to check the string representation """
@@ -54,6 +44,6 @@ class TestAmenity(unittest.TestCase):
         a1.save()
         a1_json = a1.to_dict()
         updated = a1.updated_at
-        updated2 = datetime.strptime(c1_json["updated_at"],
+        updated2 = datetime.strptime(a1_json["updated_at"],
                                      "%Y-%m-%dT%H:%M:%S.%f")
         self.assertEqual(updated, updated2)

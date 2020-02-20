1baf4893619d849c3873d611758cce6a2237985e
ariana124 <ariana.bibiano@gmail.com>
2020-02-20 03:32:41 +0000

    fixed set up and tear down of user instance

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,41 +11,37 @@ class TestUser(unittest.TestCase):
 
     def setUp(self):
         """ sets up an instance of a User """
-        self.user = User()
+        self.u1 = User()
 
     def tearDown(self):
         """ tears down an instance of a User """
-        del self.user
+        del self.u1
 
     def test_diff_id(self):
         """ tests to make sure both instances have different ids """
-        u1 = User()
         u2 = User()
-        self.assertNotEqual(u1.id, u2.id)
+        self.assertNotEqual(self.u1.id, u2.id)
 
     def test_attributes(self):
         """ tests attributes to make sure they are string """
-        u1 = User()
-        self.assertIsInstance(u1.email, str)
-        self.assertIsInstance(u1.password, str)
-        self.assertIsInstance(u1.first_name, str)
-        self.assertIsInstance(u1.last_name, str)
+        self.assertIsInstance(self.u1.email, str)
+        self.assertIsInstance(self.u1.password, str)
+        self.assertIsInstance(self.u1.first_name, str)
+        self.assertIsInstance(self.u1.last_name, str)
 
     def test_str(self):
         """ test to check the string representation """
-        u1 = User()
-        u1.first_name = "Pepe"
-        string = "[{}] ({}) {}".format(u1.__class__.__name__,
-                                       u1.id,
-                                       u1.__dict__)
-        self.assertEqual(str(u1), string)
+        self.u1.first_name = "Pepe"
+        string = "[{}] ({}) {}".format(self.u1.__class__.__name__,
+                                       self.u1.id,
+                                       self.u1.__dict__)
+        self.assertEqual(str(self.u1), string)
 
     def test_format(self):
         """ test to check for time format """
-        u1 = User()
-        u1.save()
-        u1_json = u1.to_dict()
-        updated = u1.updated_at
+        self.u1.save()
+        u1_json = self.u1.to_dict()
+        updated = self.u1.updated_at
         updated2 = datetime.strptime(u1_json["updated_at"],
                                      "%Y-%m-%dT%H:%M:%S.%f")
         self.assertEqual(updated, updated2)
